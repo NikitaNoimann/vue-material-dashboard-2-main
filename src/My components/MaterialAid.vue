71adc6345704d42d4e9137e60c1753bd50ef650b
Никитка <nikitanoimann@mail.ru>
2022-06-23 15:53:52 +0700

    Добавил формирование документа на материальную помощь

--- src/My components/MaterialAid.vue
+++ src/My components/MaterialAid.vue
@@ -16,25 +16,39 @@
 
       <b-form-input v-model="Cause" placeholder="Причина"></b-form-input>
       <b-button @click="save">Сохранить</b-button>
+      <b-button @click="renderDoc">Download Word Document</b-button>
   </div>
 </template>
 
 <script>
 import {store} from './MarerialAidStore'
 import {BButton} from "bootstrap-vue-3";
+
+import PizZip from "pizzip";
+import PizZipUtils from "pizzip/utils/index.js";
+
+// import JSZip from 'jszip';
+// import JSzipUtils from 'jszip-utils';
+import  saveAs  from 'file-saver';
+// import docxtemplater from "docxtemplater";
+import Docxtemplater from  "docxtemplater"
+function loadFile(url, callback)
+{
+  PizZipUtils.getBinaryContent(url, callback);
+}
 export default {
   name: "MaterialAid",
   components: {BButton},
   data() {
     return {
-        store,
+      store,
       FIO: '',
       Group: '',
       Cause: '',
       selected: null,
       options: [
-        { value: 'ИС-18', text: 'ИС-18' },
-        { value: 'ИС-19', text: 'ИС-19' }
+        {value: 'ИС-18', text: 'ИС-18'},
+        {value: 'ИС-19', text: 'ИС-19'}
       ]
     }
   },
@@ -47,11 +61,75 @@ export default {
           Cause: this.Cause
         })
         this.FIO = ''
-            this.Cause = ''
+        this.Cause = ''
       }
+    },
+
+    // getDoc() {
+    //   this.createAndSaveDocument();
+    // },
+    // loadFile(url, callback) {
+    //   JSzipUtils.getBinaryContent(url, callback);
+    // },
+    // createAndSaveDocument() {
+    //   this.loadFile('ReportTemplates/ServesMaterialAid.docx', function (error, content) {
+    //     if (error) {
+    //       throw error
+    //     };
+    //     let zip = new JSZip(content);
+    //     console.log(content)
+    //     let doc = new docxtemplater().loadZip(zip)
+    //
+    //     doc.render()
+    //     doc.setData({
+    //       FIO: this.FIO,
+    //       Group: "fdgdg",
+    //       Cause: "dfgdgf"
+    //     })
+    //
+    //     let out = doc.getZip().generate({
+    //       type:"blob",
+    //       mineType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    //
+    //     })
+    //     saveAs(out,`MyDocument.docx`);
+    //   })
+    //
+    // }
+
+
+    renderDoc() {
+      let a = this.FIO
+      let b = this.selected
+      let c = this.Cause
+      loadFile("ReportTemplates/ServesMaterialAid.docx", function (
+          error,
+          content
+      ) {
+        if (error) {
+          throw error;
+        }
+        const zip = new PizZip(content);
+        const doc = new Docxtemplater(zip, {paragraphLoop: true, linebreaks: true});
+
+        doc.setData({
+          FIO: a,
+          Group: b,
+          Cause: c
+        });
+        doc.render();
+        const out = doc.getZip().generate({
+          type: "blob",
+          mimeType:
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        });
+        // Output the document using Data-URI
+        saveAs(out, "output.docx");
+      });
     }
   }
 }
+
 </script>
 
 <style scoped>
